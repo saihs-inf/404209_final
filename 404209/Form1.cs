abb788fe6ebef9628d0f3250b9cc3112d54ab5a6
unknown <>
2017-05-03 19:09:12 +0800

    Button1

--- 404209/Form1.cs
+++ 404209/Form1.cs
@@ -21,5 +21,10 @@ namespace _404209
         {
             MessageBox.Show("404209");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello word");
+        }
     }
 }
